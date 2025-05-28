eabd814fbf9e52a108ce8e2cdfdce0b1a439c0f1
prathik8 <140915688+prathik8@users.noreply.github.com>
2025-05-28 15:32:21 +0530

    Fix Logo.png import path

--- src/Navbar/Logo.jsx
+++ src/Navbar/Logo.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import img from '../Navbar/Logo.png'
 import '../Navbar/Navbar.css'
 const Logo = () => {
   return (
          <> 
          <div className='logoimg'> 
-        <img src={img} alt='logo'></img></div>
+         </div>
          
         </>
   )
